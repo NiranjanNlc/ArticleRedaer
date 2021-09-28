52dd394d70653f7f64724831accecb2a741d1310
niranjannlc <niranjannlc10@gmail.com>
2021-09-28 13:25:29 +0800

    all sets connected

--- app/src/main/java/com/example/articlereader/view/ArticleList.kt
+++ app/src/main/java/com/example/articlereader/view/ArticleList.kt
@@ -2,12 +2,72 @@ package com.example.articlereader.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.articlereader.R
+import com.example.articlereader.databinding.ActivityArticleListBinding
+import com.example.articlereader.modal.database.ArticleDataBase
+import com.example.articlereader.modal.repo.ArticleRepo
+import com.example.articlereader.viewModal.ArticleViewModal
+import com.example.articlereader.viewModal.ViewModalFactory
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
-class ArticleList : AppCompatActivity() {
+class ArticleList : AppCompatActivity(),ArticleAdapter.ItemClickListener
+{
+    private lateinit var binding:ActivityArticleListBinding
+    private lateinit var sampleViewModal: ArticleViewModal
+    private lateinit var adapter:ArticleAdapter
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_article_list)
     }
+    private fun initaliseRecipeList() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_article_list)
+        sampleViewModal = initialiseViewModal()
+        adapter = ArticleAdapter(this,this)
+        bindData()
+        observeChange()
+        initRecyclerView()
+    }
+
+    private fun observeChange()
+    {
+        sampleViewModal.articlList.observe(this, {
+            println(" this data " + sampleViewModal.articlList.value.toString())
+            adapter.submitList(it)
+        })
+    }
+
+    private fun initialiseViewModal(): ArticleViewModal {
+        val applicationScope = CoroutineScope(SupervisorJob())
+        val database = ArticleDataBase.getDatabase(this, applicationScope)
+         val repository = ArticleRepo(database.reciepedDao())
+        val sampleViewModal = ViewModalFactory(repository).create(ArticleViewModal::class.java)
+        return sampleViewModal
+    }
+    private fun bindData()
+    {
+        binding.lifecycleOwner=this
+    }
+
+    private fun initRecyclerView()
+    {
+        print(" recycler view initiated")
+        binding.recyclerView.layoutManager = GridLayoutManager(this,2) as RecyclerView.LayoutManager?
+        // binding.recyclerView.setHasFixedSize(true)
+        binding.recyclerView.adapter=   adapter
+        binding.recyclerView.setItemViewCacheSize(4)
+        adapter.submitList(sampleViewModal.articlList.value)
+        // adapter.
+    }
+
+    override fun onItemClick(position: String)
+    {
+        
+    }
+
+
 }
